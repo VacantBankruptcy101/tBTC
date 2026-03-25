6637f91f95933927c740e0cc21d43cc6f20902ba
VacantBankruptcy101 <104495070+VacantBankruptcy101@users.noreply.github.com>
2026-03-25 01:29:54 -0700

    Create tBTCToken contract for ERC20 token
    This contract implements an ERC20 token representing Bitcoin on EVM, with minting and burning functionalities.

---
+++ tBTCToken.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.9.3/contracts/token/ERC20/ERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.9.3/contracts/access/Ownable.sol";
+
+/**
+ * @title tBTC Token
+ * @notice ERC20 token representing Bitcoin on EVM
+ */
+contract tBTCToken is ERC20, Ownable {
+    constructor() ERC20("tBTC Token", "tBTC") {
+        // Mint initial supply to deployer
+        _mint(msg.sender, 1000000 * 10 ** decimals());
+    }
+
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount);
+    }
+
+    function burn(uint256 amount) external {
+        _burn(msg.sender, amount);
+    }
+
+    function burnFrom(address account, uint256 amount) external {
+        _spendAllowance(account, msg.sender, amount);
+        _burn(account, amount);
+    }
+}
